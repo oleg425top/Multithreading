b2354943a2ef7e7b3510cdb3d1a9c0fb9f9c0c26
oleg425top <oleg59152@gmail.com>
2025-02-03 16:50:26 +0500

    Закончил 1 задание!!

---
+++ task_1.py
@@ -0,0 +1,40 @@
+import random
+import threading
+
+random_list = []
+def randomizer(random_list, event_for_set ):
+
+    for _ in range(20):
+        rand_number = random.randint(1, 100)
+        random_list.append(rand_number)
+    print(f'{random_list}')
+    event_for_set.set()
+
+
+def sum_elem(random_list, event_for_wait):
+    event_for_wait.wait()
+    print(f'Сумма всех элементов: {sum(random_list)}')
+
+
+def average(random_list, event_for_wait):
+    event_for_wait.wait()
+    result = sum(random_list)/len(random_list)
+    print(f'Сред.арифм. равно: {result}')
+
+
+event1 = threading.Event()
+
+
+thread_1 = threading.Thread(target=randomizer, args=(random_list, event1,))
+thread_2 = threading.Thread(target=sum_elem, args=(random_list, event1,))
+thread_3 = threading.Thread(target=average, args=(random_list, event1, ))
+
+thread_1.start()
+event1.set()
+thread_2.start()
+thread_3.start()
+
+thread_1.join()
+thread_2.join()
+thread_3.join()
+
